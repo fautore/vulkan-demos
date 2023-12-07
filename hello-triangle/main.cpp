6cc71ebd9864b6b3b74bf8e47f10b586a7e30951
fautore <michele.contato@protonmail.com>
2023-12-07 21:57:17 +0100

    create image view

--- hello-triangle/main.cpp
+++ hello-triangle/main.cpp
@@ -1,5 +1,6 @@
 // #include <vulkan/vulkan.h> // <-- headless
 #include <algorithm>
+#include <cstddef>
 #include <limits>
 #include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
@@ -87,6 +88,7 @@ private:
     std::vector<VkImage> swapChainImages;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
 
     void initWindow () {
         glfwInit();
@@ -101,6 +103,7 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
     }
     void mainLoop() {
         while(!glfwWindowShouldClose(window)) {
@@ -283,6 +286,23 @@ private:
         
         swapChainImageFormat = surfaceFormat.format;
         swapChainExtent = extent;
+    }
+    void createImageViews() {
+        swapChainImageViews.resize(swapChainImages.size());
+        for (size_t i = 0; i < swapChainImages.size(); i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange
+        }
     }
     VkSurfaceFormatKHR chooseSwapSurfaceFormat (const std::vector<VkSurfaceFormatKHR> &availableFormats) {
         for (const auto &availableFormat : availableFormats) {
