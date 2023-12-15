2f14b58de9731281a74f4bee24645dad259ec739
fautore <michele.contato@protonmail.com>
2023-12-15 21:17:20 +0100

    render pass setup, attachment description and subpasses

--- hello-triangle/main.cpp
+++ hello-triangle/main.cpp
@@ -119,6 +119,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createRenderPass();
         createGraphicsPipeline();
     }
     void mainLoop() {
@@ -331,6 +332,26 @@ private:
                 throw std::runtime_error("failed to create image views!");
             }
         }
+    }
+    void createRenderPass() {
+        VkAttachmentDescription colorAttachment{};
+        colorAttachment.format= swapChainImageFormat;
+        colorAttachment.samples= VK_SAMPLE_COUNT_1_BIT;
+        colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        VkAttachmentReference colorAttachmentRef{};
+        colorAttachmentRef.attachment = 0;
+        colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkSubpassDescription subpass{};
+        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments = &colorAttachmentRef;
     }
     void createGraphicsPipeline() {
         auto vertShaderCode = readFile("shaders/vertex-shader.vert.spv"); 
